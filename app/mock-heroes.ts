7de649139c29f4df55dad24ab39cf7617a32af4f
Aldo P <omnibot2k@gmail.com>
2017-01-10 20:23:13 -0500

    Update mock data

--- app/mock-heroes.ts
+++ app/mock-heroes.ts
@@ -1,15 +1,15 @@
 import { Hero } from './hero';
 
 export const HEROES: Hero[] = [
-  { id: 11, name: 'Ironman' },
-  { id: 12, name: 'Hulk' },
-  { id: 13, name: 'Thor' },
-  { id: 14, name: 'War Machine' },
-  { id: 15, name: 'The Thing' },
-  { id: 16, name: 'The Human Torch' },
-  { id: 17, name: 'Loki' },
-  { id: 18, name: 'Galactus' },
-  { id: 19, name: 'Dr. Doom' },
-  { id: 20, name: 'Spiderman' },
-  { id: 21, name: 'Bluey Bear' }
+  { id: 1, name: 'Ironman' },
+  { id: 2, name: 'Hulk' },
+  { id: 3, name: 'Thor' },
+  { id: 4, name: 'War Machine' },
+  { id: 5, name: 'The Thing' },
+  { id: 6, name: 'The Human Torch' },
+  { id: 7, name: 'Loki' },
+  { id: 8, name: 'Galactus' },
+  { id: 9, name: 'Dr. Doom' },
+  { id: 10, name: 'Spiderman' },
+  { id: 11, name: 'Bluey Bear' }
 ];
